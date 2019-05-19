de9ada139bebbf3542958b524940a7ecf1328c40
sandeeraDS <sandeerads@gmail.com>
2019-05-19 17:26:15 +0530

    split video frames

---
+++ video into frames/Split frames in a Video/VideoFrames.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture('1.mp4')
+path = 'D:/OpenCV/Scripts/Images'
+
+if cap.isOpened() == False:
+    print('ERROR FILE NOT FOUND OR WRONG CODEC USED!')
+count =1;
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret == True:
+        cv2.imwrite(str(count) + '.jpg', frame)
+    count+=1
+
+cap.release()
